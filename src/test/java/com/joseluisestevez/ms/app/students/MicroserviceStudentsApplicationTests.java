24a7d2952f31d0980fb95e1fc15211ec7f5250e9
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-09 12:12:23 +0200

    New SpringBoot project - Students

---
+++ src/test/java/com/joseluisestevez/ms/app/students/MicroserviceStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
