e8d4abd56f5607bfb39d1609334499f952a5ef0a
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-09 12:20:35 +0200

    Create Entity Student

---
+++ src/main/java/com/joseluisestevez/ms/app/students/entity/models/Student.java
@@ -0,0 +1,71 @@
+package com.joseluisestevez.ms.app.students.entity.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+}
