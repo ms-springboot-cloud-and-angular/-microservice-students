09da8a5dad63422a2bdbf9be40ea23af13e87493
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-09 12:46:49 +0200

    Add Student Repository

---
+++ src/main/java/com/joseluisestevez/ms/app/students/models/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.joseluisestevez.ms.app.students.models.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.joseluisestevez.ms.app.students.models.entity.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+}
