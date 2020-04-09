cff8af2be130dd8d807afedc3d04c61ddc15ca90
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-09 12:43:47 +0200

    Add lombok and set time in UTC

--- src/main/java/com/joseluisestevez/ms/app/students/entity/models/Student.java
+++ src/main/java/com/joseluisestevez/ms/app/students/entity/models/Student.java
@@ -1,6 +1,6 @@
 package com.joseluisestevez.ms.app.students.entity.models;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,7 +9,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "students")
 public class Student {
@@ -20,52 +29,15 @@ public class Student {
     private String name;
     private String lastname;
     private String email;
+
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "create_at")
-    private Date createAt;
+    private LocalDateTime createAt;
 
     @PrePersist
     public void prePersist() {
-        this.createAt = new Date();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
+        // You can use the @CreationTimestamp and @UpdateTimestamp attribute annotation
+        this.createAt = LocalDateTime.now();
     }
 
 }
