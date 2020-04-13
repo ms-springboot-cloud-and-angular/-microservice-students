62382360310ad772fdb13692b2bd2c0fec5e5fe2
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-13 11:08:00 +0200

    Add pagination

--- src/main/java/com/joseluisestevez/ms/app/students/models/repository/StudentRepository.java
+++ src/main/java/com/joseluisestevez/ms/app/students/models/repository/StudentRepository.java
@@ -3,11 +3,11 @@ package com.joseluisestevez.ms.app.students.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.joseluisestevez.ms.commons.students.models.entity.Student;
 
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
 
     @Query("SELECT a FROM Student a WHERE a.name LIKE %?1% or a.lastname LIKE %?1%")
     List<Student> findByNameOrLastname(String name);
